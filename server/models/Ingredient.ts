5559d8aaca9c095d795b28bdd831a5e4d0b2d000
ัYadawadee Klinsmith <72minniemy@gmail.com>
2026-09-24 17:04:07 +0700

    done

--- server/models/Ingredient.ts
+++ server/models/Ingredient.ts
@@ -1,4 +1,6 @@
-import { Schema, model, models } from 'mongoose'
+import mongoose from 'mongoose'
+
+const { Schema, model, models } = mongoose
 
 const IngredientSchema = new Schema({
   name: { type: String, required: true, unique: true },
